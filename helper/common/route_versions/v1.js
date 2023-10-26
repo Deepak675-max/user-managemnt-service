ea538ecd75b80dd7f933c48a513bb0635b0eb811
Deepak675-max <deepakkamboj.kamboj.675@gmail.com>
2023-10-26 20:43:43 +0530

    first commit

---
+++ helper/common/route_versions/v1.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const v1 = express.Router();
+
+// /** Application Core Routes */
+// const {
+//   taskRouter,
+// } = require("../../../routes/task.route");
+// v1.use("/task", taskRouter);
+
+
+module.exports = {
+  v1,
+};
